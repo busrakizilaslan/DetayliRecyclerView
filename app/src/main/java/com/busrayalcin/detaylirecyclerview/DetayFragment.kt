dc9b6e02089d93b747854af8a52f0027174f93c9
Busra Kizilaslan Yalcin <busra_kizilaslan@hotmail.com>
2022-07-07 23:59:50 +0300

    add adapter

--- app/src/main/java/com/busrayalcin/detaylirecyclerview/DetayFragment.kt
+++ app/src/main/java/com/busrayalcin/detaylirecyclerview/DetayFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.busrayalcin.detaylirecyclerview.databinding.FragmentDetayBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class DetayFragment : Fragment() {
@@ -16,6 +18,23 @@ class DetayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         tasarim = FragmentDetayBinding.inflate(inflater,container,false)
+
+
+        val bundle:DetayFragmentArgs by navArgs()
+        val gelenFilm = bundle.film
+
+        tasarim.toolbarDetay.title = gelenFilm.filmAdi
+        tasarim.ivFilmResim.setImageResource(
+            resources.getIdentifier(gelenFilm.filminResimAdi,"drawable",requireContext().packageName))
+
+        tasarim.tvFilmYonetmen.text = gelenFilm.filmYonetmen
+        tasarim.tvFilmYil.text = gelenFilm.filmYil.toString()
+        tasarim.tvFilmFiyat.text = "${gelenFilm.filmFiyat} ₺"
+
+        tasarim.btnSepeteEkle.setOnClickListener {
+            Snackbar.make(it,"${gelenFilm.filmAdi} sipariş verildi",Snackbar.LENGTH_SHORT).show()
+        }
+
         return tasarim.root
     }
 
